7f4cd38f7fd05bd0bc87d07e0d8dabdb369e9ec3
olehb <legowspace@gmail.com>
2022-12-11 18:34:23 +0200

    Initial commit

---
+++ src/main/java/ua/hillelit/homeworks/main/Main.java
@@ -0,0 +1,24 @@
+package ua.hillelit.homeworks.main;
+
+public class Main {
+
+    public double plus(double a, double b) {
+        return a+b;
+    }
+
+    public double minus(double a, double b) {
+        return a-b;
+    }
+
+    public double mult(double a, double b) {
+        return a*b;
+    }
+
+    public double divide(double a, double b) {
+        return a/b;
+    }
+
+    public double inDegree(double a, double b) {
+        return Math.pow(a, b);
+    }
+}
